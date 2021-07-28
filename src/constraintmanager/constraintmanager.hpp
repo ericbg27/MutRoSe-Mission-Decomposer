f9fbe7a91b3cc0d8e8485eaa7bae01ae27490f71
ericbg27 <ericbgil@gmail.com>
2021-07-28 12:20:28 -0300

    [Refactoring] Creating functions to take code out of generate at constraints function

--- src/constraintmanager/constraintmanager.hpp
+++ src/constraintmanager/constraintmanager.hpp
@@ -23,7 +23,13 @@ class ConstraintManager {
         void generate_at_constraints(ATGraph trimmed_mission_decomposition);
         void transform_at_constraints();
         void generate_execution_constraints();
+
         void generate_constraints_from_stacks(std::stack<std::pair<int,ATNode>>& operators_stack, std::stack<std::variant<std::pair<int,ATNode>,Constraint>>& nodes_stack, std::map<int,std::set<int>>& existing_constraints);
+        void generate_par_and_fallback_constraints(std::map<int,std::set<int>>& existing_constraints, std::stack<std::variant<std::pair<int,ATNode>,Constraint>>& current_branch_nodes_stack,
+                                                    std::stack<std::pair<int,ATNode>>& current_branch_operators_stack, constraint_type ctype);
+        void generate_seq_constraints(std::map<int,std::set<int>>& existing_constraints, std::stack<std::variant<std::pair<int,ATNode>,Constraint>>& current_branch_nodes_stack,
+                                        std::stack<std::pair<int,ATNode>>& current_branch_operators_stack, std::string last_op);
+        
         void trim_mission_constraints();
         void check_execution_constraints();
 
